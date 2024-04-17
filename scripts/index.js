4b7ccf0b64fb51c1550a466657a10ee9f4f7eb24
Ildar <isuncheleev@yandex.ru>
2024-04-17 14:55:55 +0300

    170424

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+const cardPlace = document.querySelector('.places__list');
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 
+function createCard (card) {
+    
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardPlace.append(cardElement);
+
+};
+
+
+
+
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach (item => {
+    console.log(item);
+    createCard(item);
+    
+});
