a48bda273ccc2e00a76c79eca790bf1f44551d4b
Ildar <isuncheleev@yandex.ru>
2024-05-13 15:50:06 +0300

    13052024

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,14 +1,21 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
-function createCard (card, deleteCard) {
+function createCard (card, deleteCard, likeCard, clickImage) {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true),
-          buttonDelete = cardElement.querySelector('.card__delete-button');
+          buttonDelete = cardElement.querySelector('.card__delete-button'),
+          cardLikeButton = cardElement.querySelector('.card__like-button');
     cardElement.querySelector('.card__image').src = card.link;
     cardElement.querySelector('.card__title').textContent = card.name;
     cardElement.querySelector('.card__image').alt = card.alt;
+    cardLikeButton.addEventListener('click', evt => { 
+      likeCard(evt);
+    });
     buttonDelete.addEventListener('click', evt => {
       deleteCard(evt);
     });
+    cardElement.querySelector('.card__image').addEventListener('click', clickImage);
+    
+
     return cardElement;
   }
   
@@ -16,6 +23,10 @@ function createCard (card, deleteCard) {
 function deleteCard(evt) {
   evt.target.parentElement.remove();
 }
-  
-export { createCard, deleteCard };
+
+function likeCard(evt) {
+  evt.target.classList.toggle('card__like-button_is-active');
+} 
+
+export { createCard, deleteCard, likeCard };
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@
 // @todo: Функция создания карточки
 import '../pages/index.css';
 import { initialCards } from './cards';
-import { createCard, deleteCard } from './card';
+import { createCard, deleteCard, likeCard } from './card';
 import { openModal, closeModal, allCloseModal } from './modal';
 
 const cardPlace = document.querySelector('.places__list');
@@ -12,9 +12,9 @@ const popupTypeEdit = document.querySelector('.popup_type_edit');
 const profileAddButton = document.querySelector('.profile__add-button');
 const popupTypeNewCard = document.querySelector('.popup_type_new-card'); 
 const popup = document.querySelectorAll('.popup');
-const popupClose = popupTypeNewCard.querySelector('.popup__close');
-const popupTypeImage = document.querySelector('.popup_type_image');
-const cardImage = document.querySelectorAll('.card__image');        
+// const popupClose = popupTypeNewCard.querySelector('.popup__close');
+// const popupTypeImage = document.querySelector('.popup_type_image');
+// const cardImage = document.querySelectorAll('.card__image');        
 const editForm = document.querySelector('form[name=edit-profile]');
 const inputName = editForm.querySelector('input[name=name]');
 const inputDescr = editForm.querySelector('input[name=description]');
@@ -23,6 +23,9 @@ const jobInput = document.querySelector('.profile__description');
 const newCard = document.querySelector('form[name=new-place]');
 const newName = newCard.querySelector('input[name=place-name]');
 const newLink = newCard.querySelector('input[name=link]');
+const popupTypeImage = document.querySelector('.popup_type_image');
+const popupImage = document.querySelector('.popup__image');
+const popupCaption = document.querySelector('.popup__caption');
 
 // @todo: Вывести карточки на страницу
 function addCard(cardElement) {
@@ -41,18 +44,26 @@ function handleFormSubmit(evt) {
   jobInput.textContent = inputDescr.value;
   closeModal(popupTypeEdit);
 }
- 
 
 function handleNewCard(evt) {
   evt.preventDefault(); 
   const card = {};
   card.name = newName.value;
-  card.link = newLink.link;
+  card.link = newLink.value;
   card.alt = newName.value;
   cardPlace.prepend(createCard(card, deleteCard));
   newCard.reset();
   closeModal(popupTypeNewCard);
 }
+
+function clickImage(evt) {
+  const openImage = evt.target;
+  popupImage.src = openImage.src;
+  popupCaption.textContent = openImage.closest('.card').querySelector('.card__title').textContent;
+  openModal(popupTypeImage);
+}
+
+
 
 
 
@@ -67,4 +78,4 @@ editForm.addEventListener('submit', handleFormSubmit);
 
 popup.forEach(item => allCloseModal(item));
 
-initialCards.forEach (item => addCard(createCard(item, deleteCard)));
+initialCards.forEach (item => addCard(createCard(item, deleteCard, likeCard, clickImage)));
